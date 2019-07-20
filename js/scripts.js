5e7a1349994cd476b3304e3df7e189c31f48062b
Devin Cooley <dcooley1350@gmail.com>
2019-07-19 17:54:22 -0700

    add jquery to capture user input.

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+// BUSINESS LOGIC
+
+
+
+
+
+
+
+
+
+
+// FRONT END LOGIC
+
+
+$(document).ready(function() {
+  $("#match form").submit(function(event) {
+    event.preventDefault();
+
+    var userName = $("input#userName").val();
+    var userGender = $("input#gender").val();
+    var userAgeRange = $("input#age").val();
+    var userEnergy = $("input#human-energy").val();
+    var userLocation = $("input#location").val();
+    var userSizePref = $("input#size").val();
+    var userEnergyPref = $("input#energy")
+  })
+})
